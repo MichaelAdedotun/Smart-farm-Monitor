d048d0bad7a2c542f9e32803648a71d0a64a8842
Michael Adedotun <adedotun2096@gmail.com>
2025-07-17 16:26:41 +0100

    added nuxt config

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: false },
+  css: ['~/assets/css/main.css', 'leaflet/dist/leaflet.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  runtimeConfig: {
+    public: {
+      agromonitoringKey: process.env.NUXT_PUBLIC_AGROMONITORING_API_KEY
+    }
+  },
+
+  modules: ['@pinia/nuxt'],
+})
